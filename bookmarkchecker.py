dd575f6020d22d2cadd39d6abdf77d163099e071
John <john.hoe@gmail.com>
2018-11-07 00:32:25 +0800

    self.bookmark_contents is now BOOKMARK_CONTENTS
    made bookmarkchecker object JSON serializable.
    
    need to solve "TypeError: Object of type bookmark is not JSON serializable" problem which occurs when trying to export results into a JSON file.

--- bookmarkchecker.py
+++ bookmarkchecker.py
@@ -6,9 +6,16 @@ import re
 import requests
 import json
 # import click
-from pprint import pprint
+# from pprint import pprint
+from collections import OrderedDict
 
 HTTP_CODES = [200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 422, 423, 424, 425, 426, 428, 429, 431, 444, 449, 450, 451, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 598, 599]
+
+# contents of the exported bookmark file
+BOOKMARK_CONTENTS = ''
+
+def jsonDefault(OrderedDict):
+    return OrderedDict.__dict__
 
 
 class bookmark(object):
@@ -24,8 +31,8 @@ class bookmark(object):
         # Store exception message.
         self.exception_msg = ''
 
-    def __str__(self):
-        return f"{self.name}, {self.isonline}, {self.response_code}"
+    def __repr__(self):
+        return json.dumps(self, default=jsonDefault, indent=4)
 
     def update_isonline(self, boolean):
         self.isonline = boolean
@@ -34,37 +41,37 @@ class bookmark(object):
 class bookmarkchecker(object):
     """docstring for bookmarkchecker."""
 
-    def __init__(self, bookmark_path):
+    def __init__(self, bookmark_path, export_path):
         # file path of exported bookmark file
         self.bookmark_path = bookmark_path
-        # contents of the exported bookmark file
-        self.bookmark_contents = ''
         # data structure which contains the bookmarks
         self.bookmark_repo = {}
         # id for records in bookmarkchecker's repo, always start from 0.
         self.bookmark_id = 0
         # number of bookmarks
         self.numofbookmarks = 0
+        # checker results export path
+        self.export_path = export_path
 
     def openbookmark(self):
         '''
         Open exported bookmark file.
         '''
-        # print("a")
+        global BOOKMARK_CONTENTS
         with open(self.bookmark_path, 'r') as fp:
-            self.bookmark_contents = fp.read()
+            BOOKMARK_CONTENTS = fp.read()
         fp.close()
-        # print(self.bookmark_contents)
-        return self.bookmark_contents
 
     def getbookmarklinks(self):
         '''
         Extract links from exported bookmark file and populate bookmark_repo
         '''
+        # print(BOOKMARK_CONTENTS)
         regex = "(?<=A HREF=\")(.*)(?=\" ADD_DATE)"
-        matches = re.findall(regex, self.bookmark_contents, re.MULTILINE | re.IGNORECASE)
+        matches = re.findall(regex, BOOKMARK_CONTENTS, re.MULTILINE | re.IGNORECASE)
 
         for match in matches:
+            print(match)
             self.bookmark_repo[self.bookmark_id] = bookmark(match)
             self.bookmark_id += 1
 
@@ -86,16 +93,30 @@ class bookmarkchecker(object):
             finally:
                 print('{} {}'.format(key, self.bookmark_repo[key]))
 
-    def repo_contents(self):
-        for key in self.bookmark_repo.keys():
-            print('{}, {} \n'.format(key, self.bookmark_repo[key]))
-            print('\n')
+#     def repo_contents(self):
+#         for key in self.bookmark_repo.keys():
+#             print('{}, {} \n'.format(key, self.bookmark_repo[key]))
+#             print('\n')
 
-    def export_results(self, export_path):
+    def __repr__(self):
+        return json.dumps(self, default=jsonDefault, indent=4)
+
+    def export_results(self):
         # skipping checks for now
-        with open(export_path, 'w') as json_fp:
-            json.dump(export_path, json_fp)
+        print('exporting results -\n')
+        with open(self.export_path, 'w+') as json_fp:
+            json.dump(self.bookmark_repo, json_fp)
         return True
+
+    # def check_export_path(self, export_path):
+    #     if path.isfile(export_path):
+    #         self.export_path = export_path
+    #         return True
+    #     else:
+    #         print('check_export_path: File or path not found.')
+    #         return False
+    #         raise SystemExit
+    #         raise FileNotFoundError('check_export_path: File or path not found.')
 
 
 # @click.command()
@@ -109,18 +130,23 @@ class bookmarkchecker(object):
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("blah")
+    if len(sys.argv) != 3:
+        print('Need path to bookmark file and exported json file.')
+        print('ie. ')
+        print('python3 /path/to/bookmarkchecker.py /path/to/bookmark.html /path/to/resultsfile')
         exit()
     if path.exists(sys.argv[1]):
-        bm_checker = bookmarkchecker(sys.argv[1])
-        bm_checker.openbookmark()
-        bm_checker.getbookmarklinks()
-        bm_checker.checkbookmarks()
-        bm_checker.repo_contents()
-        bm_checker.export_results('~/bookmarkchecker_results.json')
+        if path.exists(sys.argv[2]):
+            bm_checker = bookmarkchecker(sys.argv[1], sys.argv[2])
+            # bm_checker.check_export_path(result_export)
+            bm_checker.openbookmark()
+            # print("blah {}".format(BOOKMARK_CONTENTS))
+            bm_checker.getbookmarklinks()
+            bm_checker.checkbookmarks()
+            bm_checker.export_results()
+        print("{} is not valid".format(sys.argv[2]))
     else:
-        print("Path is not valid.")
+        print("{} is not valid".format(sys.argv[1]))
 
 
 if __name__ == '__main__':
